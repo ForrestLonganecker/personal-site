1f7f02200a8f6484c91b0cefd63653bc1a7c8a99
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-07-09 16:47:31 -0700

    altered siteMetadata in gatsby.config/]

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Forrest Longanecker | Software Engineer`,
+    description: `Forrest Longanecker Software Engineering portfolio website`,
+    author: `Forrest Longanecker`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
